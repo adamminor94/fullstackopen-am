434bbb0b428163d51954dd2ba16b851641d1a8b8
adamminor94 <adam.minor94@gmail.com>
2025-05-07 15:59:44 -0500

    Exercise 1.2

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -9,14 +9,14 @@ const App = () => {
   const exercises3 = 14
 
 
-  return (
-    <div>
-    <Header course={course}/>
-    <Content part1={part1} part2={part2} part3={part3} 
-      exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
-    />
-    <Total total={exercises1 + exercises2 + exercises3}/> 
-    </div>
+
+
+
+const Part = (props) => {
+  return(
+    <p>
+      {props.part} {props.exercises}
+    </p>
   )
 }
 
@@ -28,15 +28,9 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <p>
-      {props.part1} {props.exercises1}
-      </p>
-      <p>
-      {props.part2} {props.exercises2}
-      </p>
-      <p>
-      {props.part3} {props.exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
   </div>
   )
 }
@@ -44,6 +38,16 @@ const Total = (props) => {
   return(
   <p>Number of exercises: {props.total} </p>
   )
+}
+
+  return (
+    <div>
+    <Header course={course}/>
+    <Content/>
+    <Total total={exercises1 + exercises2 + exercises3}/> 
+    </div>
+  )
+
 }
 
 export default App
